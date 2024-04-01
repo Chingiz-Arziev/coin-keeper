b5d36f25343d5e9b0939c4954f7efb0afa02cfe5
Chingiz Arziev <chingiz.arziev@gmail.com>
2024-04-02 01:24:56 +0600

    received data from (Coingecko) api

---
+++ src/services/fetchCoinData.js
@@ -0,0 +1,22 @@
+import axios from "axios"
+export const fetchCoinData = async () => {
+  const API_KEY = import.meta.env.VITE_COINGECKO_API
+
+  const options = {
+    method: 'GET',
+    url: 'https://openapiv1.coinstats.app/coins',
+    params: {limit: '500'},
+    headers: {
+      accept: 'application/json',
+      'X-API-KEY': API_KEY
+    }
+  }
+
+  try {
+    const response = await axios.get('https://openapiv1.coinstats.app/coins', options)
+    return response.data
+
+  } catch (error) {
+    console.error(error)
+  }
+}

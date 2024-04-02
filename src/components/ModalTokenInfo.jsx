d5785b6b98a1f4ca3dc1697bdc96c87a313d63f4
Chingiz Arziev <chingiz.arziev@gmail.com>
2024-04-03 05:51:47 +0600

    Feat: Added information to the ModalTokenInfo.jsx

--- src/components/ModalTokenInfo.jsx
+++ src/components/ModalTokenInfo.jsx
@@ -1,9 +1,46 @@
-const ModalTokenInfo = ({selectedToken}) => {
+import {Flex, Tag, Typography, Divider} from "antd"
+
+const ModalTokenInfo = ({ selectedToken }) => {
+
   return (
-    <h2>
-      {selectedToken.name}
-    </h2>
-  );
-};
+  <>
+    <Flex align="center">
+      <img src={selectedToken.icon} alt={selectedToken.name} style={{width: 40, marginRight: 10}}/>
+      <Typography.Title level={2} style={{margin: 0}}>
+        ({selectedToken.symbol}) {selectedToken.name}
+      </Typography.Title>
+    </Flex>
+
+    <Divider />
+
+    <Typography.Paragraph>
+      <Typography.Text strong>1 hour: </Typography.Text>
+      <Tag color={selectedToken.priceChange1h > 0 ? 'green' : 'red'}>{selectedToken.priceChange1h}%</Tag>
+    </Typography.Paragraph>
+
+    <Typography.Paragraph>
+      <Typography.Text strong>1 day: </Typography.Text>
+      <Tag color={selectedToken.priceChange1d > 0 ? 'green' : 'red'}>{selectedToken.priceChange1d}%</Tag>
+    </Typography.Paragraph>
+
+    <Typography.Paragraph>
+      <Typography.Text strong>1 week: </Typography.Text>
+      <Tag color={selectedToken.priceChange1w > 0 ? 'green' : 'red'}>{selectedToken.priceChange1w}%</Tag>
+    </Typography.Paragraph>
+
+    <Divider />
+
+    <Typography.Paragraph>
+      <Typography.Text strong>Price: </Typography.Text>
+      {selectedToken.price.toFixed(2)}$
+    </Typography.Paragraph>
+
+    <Typography.Paragraph>
+      <Typography.Text strong>Market Cap: </Typography.Text>
+      {selectedToken.marketCap}$
+    </Typography.Paragraph>
+  </>
+  )
+}
 
 export default ModalTokenInfo

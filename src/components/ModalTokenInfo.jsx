2908ea24751f01504bb2856c993bbc21f3abfe78
Chingiz Arziev <chingiz.arziev@gmail.com>
2024-04-03 05:16:28 +0600

    Feat: creating ModalTokenInfo.jsx & bind modal with selected token by select component

---
+++ src/components/ModalTokenInfo.jsx
@@ -0,0 +1,9 @@
+const ModalTokenInfo = ({tokenName}) => {
+  return (
+    <h2>
+      {tokenName.name}
+    </h2>
+  );
+};
+
+export default ModalTokenInfo
--- src/components/layouts/AppHeader.jsx
+++ src/components/layouts/AppHeader.jsx
@@ -1,17 +1,61 @@
-import { Layout } from "antd"
+import { Layout, Select, Space, Button, Modal } from "antd"
+
+import {useState, useContext} from "react"
+import {CryptoContext} from "../../context/crypto-context.jsx"
+
+import ModalTokenInfo from "../ModalTokenInfo.jsx";
 
 const headerStyle = {
-  textAlign: 'center',
-  color: '#fff',
   height: 60,
-  paddingInline: 48,
-  lineHeight: '64px',
-  backgroundColor: '#4096ff',
-};
+  backgroundColor: '#fff',
+  display: 'flex',
+  justifyContent: 'space-between',
+  alignItems: 'center'
+}
+
 const AppHeader = () => {
+  const [modal, setModal] = useState(false)
+  const [tokenName, setTokenName] = useState(null)
+
+  const { tokens} = useContext(CryptoContext)
+
+  const tokenOptions = tokens.map(token => ({
+    label: token.name,
+    value: token.id,
+    icon: token.icon
+  }))
+
+  const handleSelect = (value) => {
+    setTokenName(tokens.find(token => token.id === value))
+    setModal(true)
+  }
+
   return (
     <Layout.Header style={headerStyle}>
-      Header
+      <Select
+        style={{width: 250,}}
+        onSelect={handleSelect}
+        options={tokenOptions}
+        optionRender={(option) => (
+          <Space>
+            <img
+              style={{width: 20}}
+              src={option.data.icon}
+              alt={option.data.label}
+            />
+            {option.data.label}
+          </Space>
+        )}
+      />
+      <Button type="primary">Add Asset</Button>
+
+      <Modal
+        open={modal}
+        onCancel={() => setModal(false)}
+        footer={null}
+      >
+        <ModalTokenInfo tokenName={tokenName}/>
+      </Modal>
     </Layout.Header>
   )
 }

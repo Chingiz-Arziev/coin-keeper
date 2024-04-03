40b671c2a97db18066db0938045cb4d088f85840
Chingiz Arziev <chingiz.arziev@gmail.com>
2024-04-03 17:14:15 +0600

    Feat: add select component to drawer window

--- src/components/AssetsForm.jsx
+++ src/components/AssetsForm.jsx
@@ -1,9 +1,41 @@
+import {useContext, useState} from "react"
+
+import {Select, Space} from "antd"
+
+import {CryptoContext} from "../context/crypto-context.jsx";
+
 const AssetsForm = () => {
-  return (
-    <div>
-      ADD ASSET
-    </div>
-  )
+  const {tokens} = useContext(CryptoContext)
+  const [token, setToken] = useState(null)
+
+  const tokenOptions = tokens.map(token => ({
+    label: token.name,
+    value: token.id,
+    icon: token.icon
+  }))
+
+  if(!token) {
+    return (
+      <>
+        <Select
+          style={{width: "100%"}}
+          placeholder="select token"
+          onSelect={(value) => setToken(tokens.find(token => token.id === value))}
+          options={tokenOptions}
+          optionRender={(option) => (
+            <Space>
+              <img
+                style={{width: 20}}
+                src={option.data.icon}
+                alt={option.data.label}
+              />
+              {option.data.label}
+            </Space>
+          )}
+        />
+      </>
+    )
+  }
 }
 
 export default AssetsForm
